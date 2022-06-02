ebf2e14bd514e751c03311f52d51e743799a0ebb
Maximilian-Schwarzenberg <maximilian.schwarzenberg23@ycdsbk12.ca>
2022-06-02 14:22:01 -0400

    added more screens

--- Sketch.java
+++ Sketch.java
@@ -3,14 +3,24 @@ import processing.core.PImage;
 
 public class Sketch extends PApplet {
 
+  PImage imgBackground2;
   PImage imgBackground;
   PImage imgShip;
   PImage imgLives;
-	
+
 	int playerX = 200;
   int playerY = 400;
 
-  int lives = 3;
+  int lives = 5;
+
+  /*
+  Screen 1 - Main menu
+  Screen 2 - Main game
+  Screen 3 - Game over
+  Screen 4- Difficulty chooser 
+  Screen 5 - Settings menu
+  */
+  int screen = 1;
 
   /**
    * Called once at the beginning of execution, put your size all in this method
@@ -25,6 +35,7 @@ public class Sketch extends PApplet {
    * values here i.e background, stroke, fill etc.
    */
   public void setup() {
+    imgBackground2 = loadImage("background2.jpg");
     imgBackground = loadImage("background.png");
     imgShip = loadImage("ship.png");
     imgLives = loadImage("lives.png");
@@ -36,33 +47,21 @@ public class Sketch extends PApplet {
    * Called repeatedly, anything drawn to the screen goes here
    */
   public void draw() {
-	  background(imgBackground);
-    
-    image(imgShip, playerX, playerY);
-
-     // player boundries
-    if (playerX < 0) {
-      playerX = 0;
+	  if (screen == 1) {
+      mainMenu();
     }
-    if (playerY < 0) {
-      playerY = 0;
+    if (screen == 2) {
+      mainGame();
     }
-    if (playerX > width-100) {
-      playerX = width-100;
+    if (screen == 3) {
+      gameOver();
     }
-    if (playerY > height-100) {
-      playerY = height-100;
+    if (screen == 4) {
+      difficultyChooser();
     }
-
-    /*
-    if (lives == 3) {
-      
+    if (screen == 5) {
+      settingsMenu();
     }
-
-    image(imgLives, 755, 795);
-    image(imgLives, 770, 795);
-    image(imgLives, 785, 795);
-    */
     
   }
   
@@ -70,7 +69,6 @@ public class Sketch extends PApplet {
 
   /**
   * Detects if a key on the keyboard is clicked
-  * @return if further keys are clicked it executes a condition
   */
   public void keyPressed() {
      // Shift the player
@@ -88,5 +86,167 @@ public class Sketch extends PApplet {
     }
    
  }
+
+ public void mainMenu() {
+  background(imgBackground2);
+
+  if ((mouseX >= 50 && mouseX <= 750) && (mouseY >= 300 && mouseY <= 350)) {
+    fill(0, 255, 0); // green
+    if (mousePressed == true) {
+      screen = 4;
+    }
+  }
+  else {
+    fill(255); // white
+  }
+  rect(50, 300, 700, 50);
+
+
+  if ((mouseX >= 50 && mouseX <= 750) && (mouseY >= 400 && mouseY <= 450)) {
+    fill(0, 0, 255); // blue
+  }
+  else {
+    fill(255); // white
+  }
+  rect(50, 400, 700, 50);
+
+
+  if ((mouseX >= 50 && mouseX <= 750) && (mouseY >= 500 && mouseY <= 550)) {
+    fill(255, 0, 0); // red
+  }
+  else {
+    fill(255); // white
+  }
+  rect(50, 500, 700, 50);
+
+
+ }
+
+ public void mainGame() {
+  background(imgBackground);
+    
+  image(imgShip, playerX, playerY);
+
+   // player boundries
+  if (playerX < 0) {
+    playerX = 0;
+  }
+  if (playerY < 0) {
+    playerY = 0;
+  }
+  if (playerX > width-100) {
+    playerX = width-100;
+  }
+  if (playerY > height-100) {
+    playerY = height-100;
+  }
+
+
+  if (lives == 5) {
+    image(imgLives, 500, 10);
+    image(imgLives, 560, 10);
+    image(imgLives, 620, 10);
+    image(imgLives, 680, 10);
+    image(imgLives, 740, 10);
+  }
+  else if (lives == 4) {
+    image(imgLives, 560, 10);
+    image(imgLives, 620, 10);
+    image(imgLives, 680, 10);
+    image(imgLives, 740, 10);
+  }
+  else if (lives == 3) {
+    image(imgLives, 620, 10);
+    image(imgLives, 680, 10);
+    image(imgLives, 740, 10);
+  }
+  else if (lives == 2) {
+    image(imgLives, 680, 10);
+    image(imgLives, 740, 10);
+  }
+  else if (lives == 1) {
+    image(imgLives, 740, 10);
+  }
+  else if (lives == 0) {
+    screen = 3;
+  }
+
+ }
+
+ public void gameOver() {
+  background(255, 0, 0);
+
+  if ((mouseX >= 50 && mouseX <= 750) && (mouseY >= 300 && mouseY <= 350)) {
+    fill(0, 255, 0); // green
+    if (mousePressed == true) {
+      screen = 4;
+    }
+  }
+  else {
+    fill(0); // black
+  }
+  rect(50, 300, 700, 50);
+
+
+  if ((mouseX >= 50 && mouseX <= 750) && (mouseY >= 550 && mouseY <= 600)) {
+    fill(0, 0, 255); // blue
+    if (mousePressed == true) {
+      screen = 1;
+    }
+  }
+  else {
+    fill(0); // black
+  }
+  rect(50, 550, 700, 50);
+
+ }
+
+ public void difficultyChooser() {
+  background(255);
+
+  if ((mouseX >= 50 && mouseX <= 750) && (mouseY >= 250 && mouseY <= 300)) {
+    fill(0); // black
+    if (mousePressed == true) {
+      lives = 5;
+      screen = 2;
+    }
+  }
+  else {
+    fill(0, 255, 0); // green
+  }
+  rect(50, 250, 700, 50);
+
+
+  if ((mouseX >= 50 && mouseX <= 750) && (mouseY >= 400 && mouseY <= 450)) {
+    fill(0); // black
+    if (mousePressed == true) {
+      lives = 3;
+      screen = 2;
+    }
+  }
+  else {
+    fill(255, 255, 0); // yellow
+  }
+  rect(50, 400, 700, 50);
+
+
+  if ((mouseX >= 50 && mouseX <= 750) && (mouseY >= 550 && mouseY <= 600)) {
+    fill(0); // black
+    if (mousePressed == true) {
+      lives = 1;
+      screen = 2;
+    }
+  }
+  else {
+    fill(255, 0, 0); // red
+  }
+  rect(50, 550, 700, 50);
+
+
+ }
+
+ public void settingsMenu() {
+   //here
+}
   
 }
