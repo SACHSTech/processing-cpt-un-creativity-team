f033dae2e26b3363f329cc62d1803a5e25247ea3
Maximilian-Schwarzenberg <maximilian.schwarzenberg23@ycdsbk12.ca>
2022-06-03 12:21:34 -0400

    added text to menus

--- Sketch1.java
+++ Sketch1.java
@@ -3,8 +3,9 @@ import processing.core.PImage;
 
 public class Sketch1 extends PApplet {
 
-  PImage imgBackground2;
   PImage imgBackground;
+  PImage imgBackground2;
+  PImage imgBackground3;
   PImage imgShip;
   PImage imgLives;
 
@@ -14,10 +15,10 @@ public class Sketch1 extends PApplet {
   int lives = 5;
 
   /*
-  Screen 1 - Main menu
-  Screen 2 - Main game
-  Screen 3 - Game over
-  Screen 4- Difficulty chooser 
+  Screen 1 - Main menu (default)
+  Screen 2 - Difficulty chooser 
+  Screen 3 - Main game
+  Screen 4 - Game over
   Screen 5 - Settings menu
   */
   int screen = 1;
@@ -35,12 +36,13 @@ public class Sketch1 extends PApplet {
    * values here i.e background, stroke, fill etc.
    */
   public void setup() {
-    imgBackground2 = loadImage("background2.jpg");
     imgBackground = loadImage("background.png");
+    imgBackground2 = loadImage("background2.jpg");
+    imgBackground3 = loadImage("background3.gif");
     imgShip = loadImage("ship.png");
     imgLives = loadImage("lives.png");
 
-    background(imgBackground);
+    background(0); // Black
   }
 
   /**
@@ -51,13 +53,13 @@ public class Sketch1 extends PApplet {
       mainMenu();
     }
     if (screen == 2) {
-      mainGame();
+      difficultyChooser();
     }
     if (screen == 3) {
-      gameOver();
+      mainGame();
     }
     if (screen == 4) {
-      difficultyChooser();
+      gameOver();
     }
     if (screen == 5) {
       settingsMenu();
@@ -90,34 +92,180 @@ public class Sketch1 extends PApplet {
  public void mainMenu() {
   background(imgBackground2);
 
+  fill(255); // White
+  textSize(100);
+  text("TITLE", 50, 150);
+
   if ((mouseX >= 50 && mouseX <= 750) && (mouseY >= 300 && mouseY <= 350)) {
-    fill(0, 255, 0); // green
+    fill(0, 255, 0); // Green
+    rect(50, 300, 700, 50);
+
+    fill(255); // Black
+    textSize(50);
+    text("PLAY", 55, 345);
+
     if (mousePressed == true) {
-      screen = 4;
+      screen = 2;
+    }
+  }
+  else {
+    fill(255); // Black
+    rect(50, 300, 700, 50);
+
+    fill(0, 255, 0); // Green
+    textSize(50);
+    text("PLAY", 55, 345);
+  }
+  
+
+
+  if ((mouseX >= 50 && mouseX <= 750) && (mouseY >= 400 && mouseY <= 450)) {
+    fill(0, 0, 255); // Blue
+    rect(50, 400, 700, 50);
+
+    fill(255); // White
+    textSize(50);
+    text("SETTINGS", 55, 445);
+
+    if (mousePressed == true) {
+      screen = 5;
     }
   }
   else {
     fill(255); // white
-  }
-  rect(50, 300, 700, 50);
+    rect(50, 400, 700, 50);
 
-
-  if ((mouseX >= 50 && mouseX <= 750) && (mouseY >= 400 && mouseY <= 450)) {
-    fill(0, 0, 255); // blue
+    fill(0, 0, 255); // Blue
+    textSize(50);
+    text("SETTINGS", 55, 445);
   }
-  else {
-    fill(255); // white
-  }
-  rect(50, 400, 700, 50);
+  
 
 
   if ((mouseX >= 50 && mouseX <= 750) && (mouseY >= 500 && mouseY <= 550)) {
-    fill(255, 0, 0); // red
+    fill(255, 0, 0); // Red
+    rect(50, 500, 700, 50);
+
+    fill(255); // White
+    textSize(50);
+    text("EXIT", 55, 545);
+
+    if (mousePressed == true) {
+      exit();
+    }
   }
   else {
-    fill(255); // white
+    fill(255); // White
+    rect(50, 500, 700, 50);
+
+    fill(255, 0, 0); // Red
+    textSize(50);
+    text("EXIT", 55, 545);
   }
-  rect(50, 500, 700, 50);
+  
+
+
+ }
+
+ public void difficultyChooser() {
+  background(255);
+
+  fill(0); // Black
+  textSize(75);
+  text("CHOOSE DIFFICULTY", 25, 150);
+
+  if ((mouseX >= 5 && mouseX <= 200) && (mouseY >= 5 && mouseY <= 55)) {
+    fill(0); // black
+    rect(5, 5, 150, 50);
+
+    fill(255); // White
+    textSize(50);
+    text("<--", 10, 50);
+
+    if (mousePressed == true) {
+      screen = 1;
+    }
+  }
+  else {
+    fill(255, 165, 0); // orange
+    rect(5, 5, 150, 50);
+
+    fill(0); // Black
+    textSize(50);
+    text("<--", 10, 50);
+  }
+  
+
+  if ((mouseX >= 50 && mouseX <= 750) && (mouseY >= 250 && mouseY <= 300)) {
+    fill(0); // black
+    rect(50, 250, 700, 50);
+
+    fill(0, 255, 0); // Green
+    textSize(50);
+    text("EASY (5 Lives)", 55, 295);
+
+    if (mousePressed == true) {
+      lives = 5;
+      screen = 3;
+    }
+  }
+  else {
+    fill(0, 255, 0); // Green
+    rect(50, 250, 700, 50);
+
+    fill(0); // Black
+    textSize(50);
+    text("EASY (5 Lives)", 55, 295);
+  }
+  
+
+
+  if ((mouseX >= 50 && mouseX <= 750) && (mouseY >= 400 && mouseY <= 450)) {
+    fill(0); // black
+    rect(50, 400, 700, 50);
+
+    fill(255, 255, 0); // yellow
+    textSize(50);
+    text("NORMAL (3 Lives)", 55, 445);
+    
+    if (mousePressed == true) {
+      lives = 3;
+      screen = 3;
+    }
+  }
+  else {
+    fill(255, 255, 0); // yellow
+    rect(50, 400, 700, 50);
+
+    fill(0); // Black
+    textSize(50);
+    text("NORMAL (3 Lives)", 55, 445);
+  }
+  
+
+
+  if ((mouseX >= 50 && mouseX <= 750) && (mouseY >= 550 && mouseY <= 600)) {
+    fill(0); // black
+    rect(50, 550, 700, 50);
+
+    fill(255, 0, 0); // Red
+    textSize(50);
+    text("HARD (1 Life)", 55, 595);
+
+    if (mousePressed == true) {
+      lives = 1;
+      screen = 3;
+    }
+  }
+  else {
+    fill(255, 0, 0); // Red
+    rect(50, 550, 700, 50);
+
+    fill(0); // Black
+    textSize(50);
+    text("HARD (1 Life)", 55, 595);
+  }
+  
 
 
  }
@@ -176,77 +324,86 @@ public class Sketch1 extends PApplet {
  public void gameOver() {
   background(255, 0, 0);
 
+  fill(0); // Black
+  textSize(75);
+  text("GAME OVER!", 25, 150);
+
   if ((mouseX >= 50 && mouseX <= 750) && (mouseY >= 300 && mouseY <= 350)) {
-    fill(0, 255, 0); // green
+    fill(0, 255, 0); // Green
+    rect(50, 300, 700, 50);
+
+    fill(0); // Black
+    textSize(50);
+    text("RETRY", 55, 345);
+
     if (mousePressed == true) {
       screen = 4;
     }
   }
   else {
-    fill(0); // black
+    fill(0); // Black
+    rect(50, 300, 700, 50);
+
+    fill(0, 255, 0); // Green
+    textSize(50);
+    text("RETRY", 55, 345);
   }
-  rect(50, 300, 700, 50);
+  
 
 
   if ((mouseX >= 50 && mouseX <= 750) && (mouseY >= 550 && mouseY <= 600)) {
-    fill(0, 0, 255); // blue
+    fill(0, 0, 255); // Blue
+    rect(50, 550, 700, 50);
+
+    fill(0); // Black
+    textSize(50);
+    text("MENU", 55, 595);
+
     if (mousePressed == true) {
       screen = 1;
     }
   }
   else {
-    fill(0); // black
+    fill(0); // Black
+    rect(50, 550, 700, 50);
+
+    fill(0, 0, 255); // Blue
+    textSize(50);
+    text("MENU", 55, 595);
   }
-  rect(50, 550, 700, 50);
-
- }
-
- public void difficultyChooser() {
-  background(255);
-
-  if ((mouseX >= 50 && mouseX <= 750) && (mouseY >= 250 && mouseY <= 300)) {
-    fill(0); // black
-    if (mousePressed == true) {
-      lives = 5;
-      screen = 2;
-    }
-  }
-  else {
-    fill(0, 255, 0); // green
-  }
-  rect(50, 250, 700, 50);
-
-
-  if ((mouseX >= 50 && mouseX <= 750) && (mouseY >= 400 && mouseY <= 450)) {
-    fill(0); // black
-    if (mousePressed == true) {
-      lives = 3;
-      screen = 2;
-    }
-  }
-  else {
-    fill(255, 255, 0); // yellow
-  }
-  rect(50, 400, 700, 50);
-
-
-  if ((mouseX >= 50 && mouseX <= 750) && (mouseY >= 550 && mouseY <= 600)) {
-    fill(0); // black
-    if (mousePressed == true) {
-      lives = 1;
-      screen = 2;
-    }
-  }
-  else {
-    fill(255, 0, 0); // red
-  }
-  rect(50, 550, 700, 50);
-
+  
 
  }
 
  public void settingsMenu() {
-   //here
+  background(255);
+
+  fill(0); // Black
+  textSize(75);
+  text("SETTINGS", 25, 150);
+
+  if ((mouseX >= 5 && mouseX <= 200) && (mouseY >= 5 && mouseY <= 55)) {
+    fill(0); // black
+    rect(5, 5, 150, 50);
+
+    fill(255); // White
+    textSize(50);
+    text("<--", 10, 50);
+
+    if (mousePressed == true) {
+      screen = 1;
+    }
+  }
+  else {
+    fill(255, 165, 0); // orange
+    rect(5, 5, 150, 50);
+
+    fill(0); // Black
+    textSize(50);
+    text("<--", 10, 50);
+  }
+  
+
 }
   
 }
